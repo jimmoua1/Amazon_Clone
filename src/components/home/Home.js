b33ed01b337c5d047155e7eda10545526d9f9ec5
Asterisk@DESKTOP-5V275IP <tsheejmaus@yahoo.com>
2021-10-07 16:54:39 -0700

    added styles for Product.css and some styling for Home.css, as well as refixing code for Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -13,7 +13,8 @@ function Home() {
         />
 
         <div className="home__row">
-          <Product/>
+          <Product />
+          <Product />
           {/* Product */}
         </div>
 
--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -10,6 +10,13 @@ function Product() {
             <small>$</small>
             <strong>19.99</strong>
           </p>
+          <div className="product__rating">
+            <p>⭐</p>
+          </div>
+          
+          <img src="https://images-na.ssl-images-amazon.com/images/I/5IZymoq7UnL._AC_5Y400_.jpg" alt="Product"/>
+
+          <button>Add to Basket</button>
         </div>
     </div>
   )
