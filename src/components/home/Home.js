887f234dffcf6b93220a6aed24c44a610f50346f
Asterisk@DESKTOP-5V275IP <tsheejmaus@yahoo.com>
2021-10-07 16:39:22 -0700

    adding Product onto Home component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import Product from "../product/Product";
 
 function Home() {
   return (
@@ -12,7 +13,7 @@ function Home() {
         />
 
         <div className="home__row">
-          {/* Product */}
+          <Product/>
           {/* Product */}
         </div>
 
