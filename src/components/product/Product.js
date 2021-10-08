337b8d464cf16711b3e2bad526d78c55966763a3
Asterisk@DESKTOP-5V275IP <tsheejmaus@yahoo.com>
2021-10-07 17:43:23 -0700

    added/refix css and deconstructure Product

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,25 +1,29 @@
-import React from 'react'
-import './Product.css'
+import React from "react";
+import "./Product.css";
 
-function Product() {
+function Product({ id, title, image, price, rating }) {
   return (
     <div className="product">
-        <div className="product__info">
-          <p>The lean startup</p>
-          <p className="product__price">
-            <small>$</small>
-            <strong>19.99</strong>
-          </p>
-          <div className="product__rating">
-            <p>⭐</p>
-          </div>
-          
-          <img src="https://images-na.ssl-images-amazon.com/images/I/5IZymoq7UnL._AC_5Y400_.jpg" alt="Product"/>
-
-          <button>Add to Basket</button>
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
         </div>
+      </div>
+
+      <img src={image} alt="Product_Image" />
+
+      <button>Add to Basket</button>
     </div>
-  )
+  );
 }
 
-export default Product
+export default Product;
