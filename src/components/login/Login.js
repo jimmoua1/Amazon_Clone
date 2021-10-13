fa6c23e75ffd7ccb09621a3a772e25f3823309b8
Asterisk@DESKTOP-5V275IP <tsheejmaus@yahoo.com>
2021-10-13 15:56:39 -0700

    added and finalized style and Login information

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -8,6 +8,27 @@ function Login() {
       <Link to='/'>
         <img className="login__logo" src="https://i.imgur.com/covfvPN.png" alt="amazon logo" />
       </Link>
+
+      <div className="login__container">
+        <h1>Sign-in</h1>
+
+        <form>
+          <h5>E-mail</h5>
+          <input type="text" />
+
+          <h5>Password</h5>
+          <input type="password" />
+
+          <button className="login__signInButton">Sign In</button>
+
+          <p>
+            By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
+            see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+          </p>
+
+          <button className="login__registerButton">Create your Amazon Account</button>
+        </form>
+      </div>
     </div>
   )
 }
